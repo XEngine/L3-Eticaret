3ac803bf674daac957adb72852c4c4f447b6eb17
Zeki Unal <zekiunal@gmail.com>
2015-07-14 18:56:15 +0300

    CSFix - Readability Fix.

--- application/config/sanalpos.php
+++ application/config/sanalpos.php
@@ -1,27 +1,27 @@
 <?php
 
 return array(
-	'factory' => array(
-		'halkbank' => array(
-			'url'   => ' ',
-			'3DGateway' => ' '
-			'username' => ' ',
-			'password'   => ' ',
-			'merchant_id' => ' ',
-			'threeDKey' => ' ',
-			'type' => ' '
-		),
-		'vakifbank' => array(
-			'url'   => 'sqlite',
-			'username' => 'application',
-			'password'   => '',
-			'merchant_id' => ''
-		),
-		'bankasya' => array(
-			'url'   => 'sqlite',
-			'username' => 'application',
-			'password'   => '',
-			'merchant_id' => ''
-		)
-	)
+    'factory' => array(
+        'halkbank'  => array(
+            'url'         => ' ',
+            '3DGateway'   => ' ',
+            'username'    => ' ',
+            'password'    => ' ',
+            'merchant_id' => ' ',
+            'threeDKey'   => ' ',
+            'type'        => ' '
+        ),
+        'vakifbank' => array(
+            'url'         => 'sqlite',
+            'username'    => 'application',
+            'password'    => '',
+            'merchant_id' => ''
+        ),
+        'bankasya'  => array(
+            'url'         => 'sqlite',
+            'username'    => 'application',
+            'password'    => '',
+            'merchant_id' => ''
+        )
+    )
 );
--- application/config/title.php
+++ application/config/title.php
@@ -1,54 +1,54 @@
 <?php
 return array(
-	/*
-	|--------------------------------------------------------------------------
-	| Default title
-	|--------------------------------------------------------------------------
-	|
-	| Type your default title for your website.
-	| This title will be prepended when there are more titles to concatenate with.
-	|
-	*/
-	'default_title' => 'Beyazesyapazar.com',
-	
-	/*
-	|--------------------------------------------------------------------------
-	| Default title when empty
-	|--------------------------------------------------------------------------
-	|
-	| This will be used when therer is no other title.
-	| Mainly used for front page of your website.
-	|
-	*/
-	'default_title_when_empty' => "Beyazeşyapazar.com :: Türkiye'nin Beyaz Eşya Pazarı!",
-	
-	/*
-	|--------------------------------------------------------------------------
-	| Delimiter
-	|--------------------------------------------------------------------------
-	|
-	| Titles will be concatenated using this delimiter.
-	|
-	*/
-	'delimiter' => ' | ',
-	
-	/*
-	|--------------------------------------------------------------------------
-	| Concatenating direction
-	|--------------------------------------------------------------------------
-	|
-	| Set to TRUE if you want to display your titles in reverse order.
-	|
-	| Title::put('Home');
-	| Title::put('Dashboard');
-	|
-	| when set to TRUE, Title::get() will returns:
-	|
-	| 	Dashboard :: Home
-	|
-	| Otherwise,
-	|
-	|	Home :: Dashboard
-	*/
-	'reverse' => FALSE,
+    /*
+    |--------------------------------------------------------------------------
+    | Default title
+    |--------------------------------------------------------------------------
+    |
+    | Type your default title for your website.
+    | This title will be prepended when there are more titles to concatenate with.
+    |
+    */
+    'default_title'            => 'Beyazesyapazar.com',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default title when empty
+    |--------------------------------------------------------------------------
+    |
+    | This will be used when therer is no other title.
+    | Mainly used for front page of your website.
+    |
+    */
+    'default_title_when_empty' => "Beyazeşyapazar.com :: Türkiye'nin Beyaz Eşya Pazarı!",
+
+    /*
+    |--------------------------------------------------------------------------
+    | Delimiter
+    |--------------------------------------------------------------------------
+    |
+    | Titles will be concatenated using this delimiter.
+    |
+    */
+    'delimiter'                => ' | ',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Concatenating direction
+    |--------------------------------------------------------------------------
+    |
+    | Set to TRUE if you want to display your titles in reverse order.
+    |
+    | Title::put('Home');
+    | Title::put('Dashboard');
+    |
+    | when set to TRUE, Title::get() will returns:
+    |
+    | 	Dashboard :: Home
+    |
+    | Otherwise,
+    |
+    |	Home :: Dashboard
+    */
+    'reverse'                  => FALSE,
 );
